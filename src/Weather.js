a17ba06a6bdd15ae522177b2979b2bbbe5a0f5ba
Justyna <jmwajman@gmail.com>
2021-03-01 15:49:53 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -84,7 +84,7 @@ export default function Weather(props) {
   );
 } else {
   const apiKey="48ddae8355adb729eaa13fbeedf0ff54";
-  let apiUrl=`http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     
   axios.get(apiUrl).then(handleResponse);
 
